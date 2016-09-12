f4e1e09577e4b0282b6ecfb9ee8e741048590fd5
TamGitsun <TamGitsun@DESKTOP-CNN14US>
2016-09-12 19:46:54 +0800

    first commit

---
+++ src/com/tamgitsun/test/ControllerTest.java
@@ -0,0 +1,29 @@
+package com.tamgitsun.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class ControllerTest {
+	
+	@RequestMapping("ControllerTest/index.do")
+	public String index(){
+		System.out.println("in ControllerTest/index");
+		return "test/index";
+	}
+	
+	@RequestMapping("/ControllerTest/index2.do")
+	public ModelAndView index2(){
+		System.out.println("in ControllerTest/index2");
+		return new ModelAndView(new RedirectView("../test/index.jsp"));  // 采用重定向方式跳转页面
+	}
+	
+	@RequestMapping("/ControllerTest/index3.do")
+	public ModelAndView index3(){
+		System.out.println("in ControllerTest/index3");
+		return new ModelAndView("test/index");
+	}
+
+}
